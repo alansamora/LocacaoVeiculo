6f698b0e543de2db11379700e878a38f7d95b1b3
Alan Samora Rodrigues <alan.samora@gluup.com.br>
2021-02-04 20:15:29 -0300

    Inicialização do serviço de usuário, criação dos Models e importação do framework de suporte ao sqlserver

---
+++ UsuarioMicroservice/UsuarioMicroservice/Model/Operador.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuarioMicroservice.Model
+{
+    public class Operador
+    {
+        public int Matricula { get; set; }
+    }
+}
