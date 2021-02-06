ebd5625fa0ccf448f65a324faace19d2ad5bb0b8
Alan Samora Rodrigues <alan.samora@gluup.com.br>
2021-02-05 22:59:36 -0300

    Importação do EntityFramework para conexão com o banco de dados sql, criação do migration de veiculo e criação das tabelas no banco de dados via comando de integração

---
+++ VeiculoMicroservice/VeiculoMicroservice/Migrations/20210206015328_VeiculoMigration.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VeiculoMicroservice.Migrations
+{
+    public partial class VeiculoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marcas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marcas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modelos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MarcaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modelos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modelos_Marcas_MarcaId",
+                        column: x => x.MarcaId,
+                        principalTable: "Marcas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    ValorHora = table.Column<double>(type: "float", nullable: false),
+                    Combustivel = table.Column<int>(type: "int", nullable: false),
+                    LimitePortaMalas = table.Column<int>(type: "int", nullable: false),
+                    Categoria = table.Column<int>(type: "int", nullable: false),
+                    MarcaId = table.Column<int>(type: "int", nullable: true),
+                    ModeloId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Veiculos_Marcas_MarcaId",
+                        column: x => x.MarcaId,
+                        principalTable: "Marcas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Veiculos_Modelos_ModeloId",
+                        column: x => x.ModeloId,
+                        principalTable: "Modelos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modelos_MarcaId",
+                table: "Modelos",
+                column: "MarcaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_MarcaId",
+                table: "Veiculos",
+                column: "MarcaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_ModeloId",
+                table: "Veiculos",
+                column: "ModeloId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+
+            migrationBuilder.DropTable(
+                name: "Modelos");
+
+            migrationBuilder.DropTable(
+                name: "Marcas");
+        }
+    }
+}
