d7f5144dd8bb43cbb9e7b3c1f0b8a1d251826bcc
Alan Samora Rodrigues <alan.samora@gluup.com.br>
2021-02-05 21:44:04 -0300

    Importação do framework de suporte ao sqlserver e criação do context do veiculo

---
+++ VeiculoMicroservice/VeiculoMicroservice/DBContexts/VeiculoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VeiculoMicroservice.Model;
+
+namespace VeiculoMicroservice.DBContexts
+{
+    public class VeiculoContext : DbContext
+    {
+        public VeiculoContext(DbContextOptions<VeiculoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Modelo> Modelos { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
